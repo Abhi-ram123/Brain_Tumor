91bef9ab545145d41a0e1815189aaafbd04da32f
Abhi-ram123 <abhiramreddymalatatagari@gmail.com>
2026-06-11 10:51:16 +0530

    updated19

--- src/config.py
+++ src/config.py
@@ -26,7 +26,7 @@ TUMOR_RAW=Path(r"C:\MTECH\ROP\multi-modal-mri-and-mask-synthesis-with-conditiona
 # Processed data folders
 IXI_SLICES=PROCESSED_DIR / "ixi_slices"
 TUMOR_PROCESSED=PROCESSED_DIR / "tumor_images_processed"
-SYN_IXI_SLICES = PROCESSED_DIR / "synthetic_ixi"
+SYN_IXI_SLICES=PROCESSED_DIR / "synthetic_ixi"
 DENOISED_TUMOR = PROCESSED_DIR / "tumor_images_denoised"
 
 # Settings
